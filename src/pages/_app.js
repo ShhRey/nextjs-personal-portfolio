dc2910ee6ca775b9351b025970171904041e9a11
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-01-14 13:36:01 -0500

    Installing Dependencies. Closes #3

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <PrimeReactProvider>
+            <Component {...pageProps} />
+        </PrimeReactProvider>
+    )
 }
