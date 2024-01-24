5f2cfad067cee61d3f54123c03f5284233d7793f
Shrey Trivedi <shreytrivedi577@gmail.com>
2024-01-24 01:57:18 -0500

    Added content for Contact Page.

---
+++ src/pages/contact.js
@@ -0,0 +1,47 @@
+import NavBar from "@/components/NavBar";
+import { useRouter } from 'next/router';
+import { Divider } from "primereact/divider";
+import { Image } from "primereact/image";
+
+export default function Contact() {
+    const connectHeader = "Want To Connect?"
+    
+    const router = useRouter();
+
+    const onElementClick = (elementID) => {
+		switch(elementID) {
+			case 'home':
+				router.push('/');
+				break;
+			case 'about':
+				router.push('/about');
+				break;
+			case 'journey':
+				router.push('/journey');
+				break;
+			case 'contact':
+				router.push('/contact');
+		}
+	}
+
+    return (
+        <>
+            <div className="px-8 overflow-x-hidden">
+                <NavBar onMenuClick={onElementClick} />
+                <div id="connContainer" className="flex flex-column justify-content-center flex-wrap">
+                    <h1 className="flex justify-content-center flex-wrap">{connectHeader}</h1>
+                    <div id="connContent" className="flex justify-content-center flex-wrap gap-4 pt-3">
+                        <div id="connLeftSide" className="flex flex-column ">
+                            <p>Need Help?</p>
+                            <p>Email me at: shreytrivedi577@gmail.com</p>
+                        </div>
+                        <Divider layout="vertical" />
+                        <div id="connRightSide" className="">
+                            <Image src="path" alt="google-maps" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
